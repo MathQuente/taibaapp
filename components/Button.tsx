f002fd449ecdae6c74bf965f343c96fa298d5840
Matheus de Sousa <math.de.sousa.silva@gmail.com>
2024-10-30 16:38:36 -0300

    switch to children element

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,24 +1,24 @@
-import { forwardRef } from 'react';
+import { forwardRef, ReactNode } from 'react';
 import { Text, TouchableOpacity, TouchableOpacityProps } from 'react-native';
 
 type ButtonProps = {
-  title: string;
+  children: ReactNode;
 } & TouchableOpacityProps;
 
 export const Button = forwardRef<TouchableOpacity, ButtonProps>(
-  ({ title, ...touchableProps }, ref) => {
+  ({ children, ...touchableProps }, ref) => {
     return (
       <TouchableOpacity
         ref={ref}
         {...touchableProps}
         className={`${styles.button} ${touchableProps.className}`}>
-        <Text className={styles.buttonText}>{title}</Text>
+        <Text className={styles.buttonText}>{children}</Text>
       </TouchableOpacity>
     );
   }
 );
 
 const styles = {
-  button: 'items-center bg-indigo-500 rounded-[28px] shadow-md p-4',
+  button: 'items-center bg-white rounded-full shadow-md p-3 absolute bottom-4 right-8',
   buttonText: 'text-white text-lg font-semibold text-center',
 };
