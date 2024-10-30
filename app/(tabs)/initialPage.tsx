3eb831c33b3ae4d7b78d71366042de781f04e2ad
Matheus de Sousa <math.de.sousa.silva@gmail.com>
2024-10-30 16:37:23 -0300

    create screen with button and scroll

--- app/(tabs)/initialPage.tsx
+++ app/(tabs)/initialPage.tsx
@@ -1,13 +1,60 @@
 /* eslint-disable prettier/prettier */
-import { Text, FlatList } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import { Text, FlatList, View } from 'react-native';
+
+import { Button } from '~/components/Button';
 
 export default function Home() {
   return (
-    <FlatList
-      renderItem={() => <Text>Sexo</Text>}
-      data={[0, 1, 2, 3]}
-      keyExtractor={(_, index) => index.toString()}
-      horizontal
-    />
+    <View className="flex-1 bg-white">
+      <View className="bg-[#CCCCCC] p-4">
+        <Text className="pb-2 pl-5 text-lg">New Events</Text>
+        <FlatList
+          data={[0, 1, 2]}
+          keyExtractor={(_, index) => index.toString()}
+          numColumns={3}
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{
+            paddingHorizontal: 16,
+          }}
+          columnWrapperStyle={{
+            flex: 1,
+            justifyContent: 'space-between',
+          }}
+          renderItem={() => (
+            <View className="h-30 mr-3 flex-1">
+              <View className="h-28 bg-[#D9D9D9]" />
+              <View>
+                <Text className="text-lg font-bold">Nome</Text>
+                <Text className="text-lg">Local</Text>
+                <Text className="text-lg">Horário</Text>
+              </View>
+            </View>
+          )}
+        />
+      </View>
+      <View className="flex-1 px-4">
+        <Text className="pb-2 pt-2 text-lg">New Places</Text>
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          data={Array.from({ length: 5 })}
+          keyExtractor={(_, index) => index.toString()}
+          renderItem={() => (
+            <View className="py- flex flex-row justify-between bg-[#CCCCCC] px-3 py-4">
+              <View>
+                <Text className="font-semibold">Nome do Local</Text>
+                <Text>Endereço</Text>
+                <Text>Horário de Funcionamento</Text>
+                <Text>Dias de Funcionamento</Text>
+              </View>
+              <View className="h-24 w-24 bg-black" />
+            </View>
+          )}
+        />
+      </View>
+      <Button>
+        <MaterialIcons name="search" size={24} color="black" />
+      </Button>
+    </View>
   );
 }
