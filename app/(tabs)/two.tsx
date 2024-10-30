86cefc6185fdc3bad69ab9bf19f614208647b050
Matheus de Sousa <math.de.sousa.silva@gmail.com>
2024-10-30 20:14:48 -0300

    make place screen

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,22 +1,38 @@
-import { Stack } from 'expo-router';
-import { StyleSheet, View } from 'react-native';
+import { FlatList, Text, TouchableOpacity, View } from 'react-native';
 
-import { ScreenContent } from '~/components/ScreenContent';
+import { Button } from '~/components/Button';
 
-export default function Home() {
+export default function Places() {
   return (
-    <>
-      <Stack.Screen options={{ title: 'Tab Two' }} />
-      <View style={styles.container}>
-        <ScreenContent path="app/(tabs)/two.tsx" title="Tab Two" />
+    <View className="flex-1">
+      <View className="flex-row items-center justify-between px-6 py-3">
+        <Text className="text-lg">Places</Text>
+        <View className="w-28">
+          <TouchableOpacity className="flex items-center bg-[#CCCCCC] p-3">
+            <Text>Filter</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </>
+      <View className="flex-1 px-7">
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          data={Array.from({ length: 5 })}
+          keyExtractor={(_, index) => index.toString()}
+          style={{ flex: 1, backgroundColor: '#CCCCCC' }}
+          renderItem={() => (
+            <View className="flex-row justify-between bg-[#CCCCCC] px-3 py-4">
+              <View>
+                <Text className="font-semibold">Nome do Local</Text>
+                <Text>Endereço</Text>
+                <Text>Horário de Funcionamento</Text>
+                <Text>Dias de Funcionamento</Text>
+              </View>
+              <View className="h-24 w-24 bg-black" />
+            </View>
+          )}
+        />
+      </View>
+      <Button />
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-  },
-});
