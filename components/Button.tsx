2ad6244204fec7b3643296ba64e03ee79c4dd1e6
Matheus de Sousa <math.de.sousa.silva@gmail.com>
2024-10-30 20:15:24 -0300

    change button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,24 +1,18 @@
-import { forwardRef, ReactNode } from 'react';
-import { Text, TouchableOpacity, TouchableOpacityProps } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import { forwardRef } from 'react';
+import { Text, TouchableOpacity } from 'react-native';
 
-type ButtonProps = {
-  children: ReactNode;
-} & TouchableOpacityProps;
-
-export const Button = forwardRef<TouchableOpacity, ButtonProps>(
-  ({ children, ...touchableProps }, ref) => {
-    return (
-      <TouchableOpacity
-        ref={ref}
-        {...touchableProps}
-        className={`${styles.button} ${touchableProps.className}`}>
-        <Text className={styles.buttonText}>{children}</Text>
-      </TouchableOpacity>
-    );
-  }
-);
+export const Button = forwardRef<TouchableOpacity>(({ ...touchableProps }, ref) => {
+  return (
+    <TouchableOpacity ref={ref} {...touchableProps} className={`${styles.button} `}>
+      <Text className={styles.buttonText}>
+        <MaterialIcons name="search" size={24} color="black" />
+      </Text>
+    </TouchableOpacity>
+  );
+});
 
 const styles = {
-  button: 'items-center bg-white rounded-full shadow-md p-3 absolute bottom-4 right-8',
+  button: 'items-center bg-white rounded-full shadow-lg p-4 absolute bottom-4 right-2',
   buttonText: 'text-white text-lg font-semibold text-center',
 };
