9934aeb9ece2f155d1dee759c053743ff8ef1974
Matheus de Sousa <math.de.sousa.silva@gmail.com>
2024-10-31 18:03:17 -0300

    fix line height and padding

--- app/(tabs)/eventDetails.tsx
+++ app/(tabs)/eventDetails.tsx
@@ -7,19 +7,19 @@ export default function eventDetails() {
   return (
     <View className="flex-1">
       <View className="h-2/5 bg-[#FFFF00]" />
-      <View className="flex h-3/5 flex-1 flex-col bg-[#BEBEBE] px-10 py-5">
-        <View className="flex flex-col gap-4">
+      <View className="flex h-3/5 flex-1 flex-col justify-evenly bg-[#BEBEBE] px-8 py-5">
+        <View className="flex flex-col gap-3">
           <Text className="text-3xl font-bold">Place/Event Name</Text>
-          <Text className="text-lg font-semibold">
+          <Text className="ml-1.5 text-lg font-semibold leading-none">
             Lorem Ipsum Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem
             Ipsum(Descrição)
           </Text>
-          <View className="leading-none">
-            <Text className="text-lg font-semibold">Horário:</Text>
-            <Text className="text-lg font-semibold">Data:</Text>
+          <View className="ml-1.5">
+            <Text className="text-lg font-semibold leading-none">Horário:</Text>
+            <Text className="text-lg font-semibold leading-none">Data:</Text>
           </View>
         </View>
-        <View className="h-56 w-full bg-[#FF00FF]" />
+        <View className="ml-1.5 h-56 w-full bg-[#FF00FF]" />
       </View>
       <Button />
     </View>
