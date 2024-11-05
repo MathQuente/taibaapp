0839b4215b87b6f20300493a1b5f94723eb36f4b
Matheus de Sousa <math.de.sousa.silva@gmail.com>
2024-11-05 16:45:30 -0300

    react query implementation to consume api

--- app/(tabs)/mainPage.tsx
+++ app/(tabs)/mainPage.tsx
@@ -1,33 +1,49 @@
 /* eslint-disable prettier/prettier */
 
-import { Text, FlatList, View } from 'react-native';
+import { useQuery } from '@tanstack/react-query';
+import { format } from 'date-fns';
+import { Text, FlatList, View, Image } from 'react-native';
 
+import { useApi } from '~/api/useApi';
 import { Button } from '~/components/Button';
+import { HomeResponse } from '~/types/responseTypes';
 
 export default function Home() {
+  const api = useApi();
+
+  const { data: HomeResponse } = useQuery<HomeResponse>({
+    queryKey: ['home'],
+    queryFn: async () => api.getHomeInfo(),
+  });
+
   return (
     <View className="flex-1 bg-white">
       <View className="bg-[#CCCCCC] p-4">
-        <Text className="ml-3.5 pb-2 text-lg">New Events</Text>
+        <Text className="pb-2 pl-5 text-lg">New Events</Text>
         <FlatList
-          data={[0, 1, 2]}
+          data={HomeResponse?.events}
           keyExtractor={(_, index) => index.toString()}
           numColumns={3}
           showsHorizontalScrollIndicator={false}
           contentContainerStyle={{
-            paddingHorizontal: 12,
+            paddingHorizontal: 16,
           }}
           columnWrapperStyle={{
             flex: 1,
             justifyContent: 'space-between',
           }}
-          renderItem={() => (
+          renderItem={({ item }) => (
             <View className="h-30 mr-3 flex-1">
-              <View className="h-28 bg-[#D9D9D9]" />
+              <Image
+                className="size-28"
+                source={{
+                  uri: 'https://res.cloudinary.com/dtdkzusmw/image/upload/v1727547577/images/yjoka1rbduygfwhxczm9.gif',
+                }}
+              />
               <View>
-                <Text className="text-lg font-bold">Nome</Text>
-                <Text className="text-lg">Local</Text>
-                <Text className="text-lg">Horário</Text>
+                <Text className="text-lg font-bold">{item.name}</Text>
+                <Text className="text-lg">{item.address}</Text>
+                <Text className="text-lg">{format(new Date(item.date), 'dd/MM/yyyy')}</Text>
               </View>
             </View>
           )}
@@ -37,17 +53,22 @@ export default function Home() {
         <Text className="pb-2 pt-2 text-lg">New Places</Text>
         <FlatList
           showsVerticalScrollIndicator={false}
-          data={Array.from({ length: 5 })}
+          data={HomeResponse?.places}
           keyExtractor={(_, index) => index.toString()}
-          renderItem={() => (
+          renderItem={({ item }) => (
             <View className=" flex flex-row justify-between bg-[#CCCCCC] px-3 py-4">
               <View>
-                <Text className="font-semibold">Nome do Local</Text>
-                <Text>Endereço</Text>
-                <Text>Horário de Funcionamento</Text>
-                <Text>Dias de Funcionamento</Text>
+                <Text className="font-semibold">{item.name}</Text>
+                <Text>{item.address}</Text>
+                <Text>{item.working_hours}</Text>
+                <Text>{item.working_days}</Text>
               </View>
-              <View className="h-24 w-24 bg-black" />
+              <Image
+                className="size-28"
+                source={{
+                  uri: 'https://res.cloudinary.com/dtdkzusmw/image/upload/v1727547577/images/yjoka1rbduygfwhxczm9.gif',
+                }}
+              />
             </View>
           )}
         />
