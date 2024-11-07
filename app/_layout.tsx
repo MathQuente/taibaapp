e185456d8a7b2e3697d334fe2731f6cf1f4f786a
Matheus de Sousa <math.de.sousa.silva@gmail.com>
2024-11-07 16:36:34 -0300

    add queryClient and QueryClientProvider to wrap aplication

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,28 @@
+/* eslint-disable prettier/prettier */
+
 import '../global.css';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Stack } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
+const queryClient = new QueryClient();
+
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
+
   initialRouteName: '(tabs)',
 };
 
 export default function RootLayout() {
   return (
-    <SafeAreaView className="flex-1">
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </SafeAreaView>
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaView className="flex-1">
+        <Stack initialRouteName="(tabs)">
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+      </SafeAreaView>
+    </QueryClientProvider>
   );
 }
+
