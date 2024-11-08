70512b2f3ec871b5a4bb2cbe2906afaa53215796
Matheus de Sousa <math.de.sousa.silva@gmail.com>
2024-11-08 20:36:33 -0300

    bug: fix the bug that make keyboard push all content to off SafeAreaView

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,24 +4,29 @@ import '../global.css';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Stack } from 'expo-router';
+import { KeyboardAvoidingView, Platform } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const queryClient = new QueryClient();
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-
   initialRouteName: '(tabs)',
 };
 
 export default function RootLayout() {
   return (
     <QueryClientProvider client={queryClient}>
-      <SafeAreaView className="flex-1">
-        <Stack initialRouteName="(tabs)">
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        </Stack>
-      </SafeAreaView>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={{ flex: 1 }}
+        enabled={false}>
+        <SafeAreaView className="flex-1">
+          <Stack initialRouteName="(tabs)">
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          </Stack>
+        </SafeAreaView>
+      </KeyboardAvoidingView>
     </QueryClientProvider>
   );
 }
